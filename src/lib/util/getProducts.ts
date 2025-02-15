e1cb45e18bf09f2b53afebfce66b34e897c73a87
Franco Lucero <frlcr@icloud.com>
2025-02-15 15:37:35 -0300

    getProducts.ts fixed

--- src/lib/util/getProducts.ts
+++ src/lib/util/getProducts.ts
@@ -1,6 +1,8 @@
 export const getProducts = async () => {
     try {
         const backendUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL;
+        console.log("backendUrl:", backendUrl);
+        console.log("process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY);
         const response = await fetch(`${backendUrl}/store/products`, {
             method: "GET",
             mode: "cors",
